958dbe9a8e0d53ca8764b7270f074d22a45f2779
elhalj <wilsonikedakoffi7@gmail.com>
2025-02-28 01:16:48 +0000

    add licence

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL.split(",") || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
