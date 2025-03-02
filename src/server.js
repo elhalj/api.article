9bdaed733a76d1c47f8d9bbb46a2672cbb71bf08
WILSON IKEDA <wilsonikedakoffi7@gmail.com>
2025-03-02 22:17:19 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import { articleRoute } from "./routes/article.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
