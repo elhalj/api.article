bffb50f3113cb0ded651956bd62812691950fadb
WILSON IKEDA <wilsonikedakoffi7@gmail.com>
2025-03-10 23:29:37 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,12 +14,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'http://localhost:5173', // Autoriser uniquement votre frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use("/api", routes);
 app.use("/api/article", articleRoute);
